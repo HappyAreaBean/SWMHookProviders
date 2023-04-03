2c36c1e03428fd0e7e3a4f1d8bdcb9e36ce00006
HappyAreaBean <bean@happyareabean.cc>
2023-04-03 10:41:48 +0800

    feat: Initial commit
    This is the initial commit of the project, which includes all the files and directories necessary to get started.

---
+++ Test/src/main/java/cc/happyareabean/swmhook/provider/TestArenaProvider.java
@@ -0,0 +1,28 @@
+package cc.happyareabean.swmhook.provider;
+
+import cc.happyareabean.swmhook.hook.ArenaProvider;
+import cc.happyareabean.swmhook.objects.SWMHWorld;
+import org.bukkit.World;
+
+public class TestArenaProvider extends ArenaProvider {
+
+	@Override
+	public void addArena(SWMHWorld world) {
+
+	}
+
+	@Override
+	public void removeArena(SWMHWorld world) {
+
+	}
+
+	@Override
+	public String getProviderName() {
+		return "TestArenaProvider";
+	}
+
+	@Override
+	public String getProviderFileName() {
+		return "arenas.yml";
+	}
+}
